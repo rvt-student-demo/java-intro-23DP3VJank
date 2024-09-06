8993f9e81c678b30942a4f64461804726f74ea4c
23DP3VJank <166131816+23DP3VJank@users.noreply.github.com>
2024-09-06 11:35:12 +0000

    Uzdevums

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,41 @@
 package lv.rvt;
+import java.nio.file.WatchEvent;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Ievadi savu vārdu");
+        String vards = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvardu");
+        String uzvards = scanner.nextLine();
+
+        System.out.println("Ievadi savu grupu");
+        String grupa = scanner.nextLine();
+
+        System.out.println("Students: " + vards + " " + uzvards + ", grupa: " + grupa);
+
+    
+        
+        
+    
+        
+        
+        // String input = scanner.nextLine();
+        // System.out.println("Sveiks " + input + "!");
+
+
+
+        // String name = "John";
+        // int age = 20;
+        // boolean is0line = false;
+               
+        // System.out.println ("is0lines!");
+
 
     }
 
