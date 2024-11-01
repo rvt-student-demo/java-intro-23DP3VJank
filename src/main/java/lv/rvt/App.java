81df010aec321804d62f132503a6204718a8e4bf
23DP3VJank <166131816+23DP3VJank@users.noreply.github.com>
2024-11-01 06:57:47 +0000

    test

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,36 +6,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
+        int x=10;
+		System.out.println(123);
+        // while (x<100) {
+        //   System.out.println(x);
+          
 
-        System.out.println("Ievadi savu vārdu");
-        String vards = scanner.nextLine();
-
-        System.out.println("Ievadi savu uzvardu");
-        String uzvards = scanner.nextLine();
-
-        System.out.println("Ievadi savu grupu");
-        String grupa = scanner.nextLine();
-
-        System.out.println("Students: " + vards + " " + uzvards + ", grupa: " + grupa);
-
-    
-        
-        
-    
-        
-        
-        // String input = scanner.nextLine();
-        // System.out.println("Sveiks " + input + "!");
-
-
-
-        // String name = "John";
-        // int age = 20;
-        // boolean is0line = false;
-               
-        // System.out.println ("is0lines!");
-
+        // }
 
     }
 
